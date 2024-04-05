a0e36530606493cd230e5c3b1faac1c41510f4bb
unknown <shivamnainwal63@gmail.com>
2024-04-05 10:37:29 +0530

    changes made

---
+++ src/Components/ProductDisplay/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function ProductDisplay({title, image, price}) {
+  return (
+    <div className="p-2 lg:p-4 w-2/4 h-full lg:h-full lg:w-1/4">
+            <div className="h-full border border-gray-300 rounded-xl overflow-hidden shadow-md cursor-pointer">
+                    <img
+                        className="lg:h-80  h-36 lg:w-96 w-full"
+                        src={image}
+                        alt="blog"
+                    />
+                <div className="p-6">
+                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                        T-Center
+                    </h2>
+                    <h1 className="title-font text-xs lg:text-lg font-medium text-gray-900 mb-3">
+                        {title.substring(0, 25)}
+                    </h1>
+                    <h1 className="title-font text-xs lg:text-lg font-medium text-gray-900 mb-3">
+                        ₹{price}
+                    </h1>
+
+                    <div className="flex justify-center ">
+                        <button className=" bg-yellow-400 hover:bg-yellow-300 text-black w-full py-[4px] rounded-lg font-bold">
+                            Add To Cart
+                        </button>
+                    </div>
+                </div>
+            </div>
+    </div>
+  )
+}
+
+export default ProductDisplay
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import Layout from '../../Components/Layout';
 import HeroSection from '../../Components/HeroSection';
 import Category from '../../Components/Category';
+import HomePageProduct from '../../Components/HomePageProduct';
+import Track from '../../Components/Track';
 function Home() {
   return (
     <Layout>
         <HeroSection />
         <Category />
+        <HomePageProduct />
+        <Track />
     </Layout>
   )
 }
